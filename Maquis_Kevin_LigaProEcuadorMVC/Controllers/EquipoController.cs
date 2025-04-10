95b6ea97f29a0f3661a930273418275f5a44b5d2
JohanKYT <kevin.maquis@udla.edu.ec>
2025-04-10 08:21:43 -0500

    Creacion de la vista Tabla de Posiciones Y Mejora de Botones

---
+++ Maquis_Kevin_LigaProEcuadorMVC/Controllers/EquipoController.cs
@@ -0,0 +1,47 @@
+﻿using Maquis_Kevin_LigaProEcuadorMVC.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Maquis_Kevin_LigaProEcuadorMVC.Controllers
+{
+    public class EquipoController : Controller
+    {
+        public IActionResult ListaEquipos()
+        {
+            List<Equipo> equipos = new List<Equipo>();
+            Equipo LDU = new Equipo()
+            {
+                ID=1,
+                Nombre = "LDU",
+                PartidosJugados = 10,
+                PartidosGanados = 5,
+                PartidosEmpatados = 3,
+                PartidosPerdidos = 2
+            };
+            equipos.Add(LDU);
+
+            Equipo Barcelona = new Equipo()
+            {
+                ID = 2,
+                Nombre = "Barcelona",
+                PartidosJugados = 10,
+                PartidosGanados = 6,
+                PartidosEmpatados = 2,
+                PartidosPerdidos = 2
+            };
+            equipos.Add(Barcelona);
+
+            Equipo Emelec = new Equipo()
+            {
+                ID = 2,
+                Nombre = "Emelec",
+                PartidosJugados = 10,
+                PartidosGanados = 4,
+                PartidosEmpatados = 4,
+                PartidosPerdidos = 2
+            };
+            equipos.Add(Emelec);
+
+            return View(equipos);
+        }
+    }
+}
--- Maquis_Kevin_LigaProEcuadorMVC/Models/Equipo.cs
+++ Maquis_Kevin_LigaProEcuadorMVC/Models/Equipo.cs
@@ -1,14 +1,35 @@
-﻿namespace Maquis_Kevin_LigaProEcuadorMVC.Models
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace Maquis_Kevin_LigaProEcuadorMVC.Models
 {
     public class Equipo
     {
+        [Key]
         public int ID { get; set; }
+        [MaxLength(100)]
+        [DisplayName("Nombre Equipo: ")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio.")]
+        //public string? Nombre { get; set; } : "?" Significa que puede ser nulo
         public string Nombre { get; set; }
+        [Range(0,20)]
         public int PartidosJugados { get; set; }
+        [Range(0, 20)]
         public int PartidosGanados { get; set; }
+        [Range(0, 20)]
         public int PartidosEmpatados { get; set; }
+        [Range(0, 20)]
         public int PartidosPerdidos { get; set; }
-        public int Puntos { get; set; }
+        [Range(0, 20)]
+        public int Puntos
+        {
+            get
+            {
+                int puntos = (PartidosGanados * 3) + PartidosEmpatados * 1;
+                return puntos;
+            }
+        }
+
 
     }
 }

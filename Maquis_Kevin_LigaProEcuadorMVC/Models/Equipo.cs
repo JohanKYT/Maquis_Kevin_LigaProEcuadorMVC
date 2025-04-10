7097d03cc98bc3bdc4eae83941133c4597e8ce9d
JohanKYT <kevin.maquis@udla.edu.ec>
2025-04-10 07:34:35 -0500

    Creacion de la clase Equipo

---
+++ Maquis_Kevin_LigaProEcuadorMVC/Models/Equipo.cs
@@ -0,0 +1,14 @@
+﻿namespace Maquis_Kevin_LigaProEcuadorMVC.Models
+{
+    public class Equipo
+    {
+        public int ID { get; set; }
+        public string Nombre { get; set; }
+        public int PartidosJugados { get; set; }
+        public int PartidosGanados { get; set; }
+        public int PartidosEmpatados { get; set; }
+        public int PartidosPerdidos { get; set; }
+        public int Puntos { get; set; }
+
+    }
+}

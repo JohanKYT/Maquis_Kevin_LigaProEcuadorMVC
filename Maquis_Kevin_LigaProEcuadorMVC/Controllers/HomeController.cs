48c59a80beaae15adf67b5c5219b373a13778399
Johan <Johan@JOHANKYT>
2025-04-10 07:22:52 -0500

    Add project files.

---
+++ Maquis_Kevin_LigaProEcuadorMVC/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+using System.Diagnostics;
+using Maquis_Kevin_LigaProEcuadorMVC.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Maquis_Kevin_LigaProEcuadorMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}

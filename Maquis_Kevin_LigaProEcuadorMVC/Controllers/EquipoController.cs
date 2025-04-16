6f1fba10d19ee37ed06331c211543fe8d036c667
JohanKYT <kevin.maquis@udla.edu.ec>
2025-04-15 21:39:53 -0500

    Ahora si se arreglo el "unreachable code"

--- Maquis_Kevin_LigaProEcuadorMVC/Controllers/EquipoController.cs
+++ Maquis_Kevin_LigaProEcuadorMVC/Controllers/EquipoController.cs
@@ -21,22 +21,9 @@ namespace Maquis_Kevin_LigaProEcuadorMVC.Controllers
         }
         public IActionResult Edit(int ID)
         {
-            Equipo LDU = new Equipo()
-            {
-                ID = 1,
-                Nombre = "LDU",
-                PartidosJugados = 10,
-                PartidosGanados = 5,
-                PartidosEmpatados = 3,
-                PartidosPerdidos = 2,
-                LOGOUrl = "https://upload.wikimedia.org/wikipedia/commons/e/e0/Liga_Deportiva_Universitaria_de_Quito.png"
-            };
-            return View(LDU);
-
             EquipoRepositorio repositorio = new EquipoRepositorio();
             var equipos = repositorio.DevuelveInfoEquipoPorID(ID);
             return View(equipos);
-
         }
     }
 }
